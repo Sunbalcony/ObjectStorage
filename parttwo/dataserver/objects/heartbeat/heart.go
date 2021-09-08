4807c891941d407c351e471bf9a7f8947a7ea870
NotByChance <160632460@qq.com>
2021-09-08 16:33:00 +0800

    Update README.md

--- parttwo/dataserver/objects/heartbeat/heart.go
+++ parttwo/dataserver/objects/heartbeat/heart.go
@@ -9,7 +9,7 @@ func StartHeartbeat() {
 	q := rabbit.New("amqp://storage:storage@mid.low.im:5672")
 	defer q.Close()
 	for {
-		q.Publish("apiServers", "1.1.1.1")
+		q.Publish("apiServers", "1.2.3.4")
 		time.Sleep(time.Second * 5)
 
 	}
