6984ca0885857cc0993679f1d93df16ed566c00f
Sunbalcony <1141789386@qq.com>
2021-09-09 00:47:27 +0800

    Update heart.go

--- parttwo/dataserver/heartbeat/heart.go
+++ parttwo/dataserver/heartbeat/heart.go
@@ -12,7 +12,7 @@ func StartHeartbeat() {
 	defer q.Close()
 	i := 0
 	for {
-		q.Publish("apiServers", "http://127.0.0.1:1234")
+		q.Publish("apiServers", "127.0.0.1:1234")
 		time.Sleep(time.Second * 5)
 		i += 1
 		fmt.Println("心跳发送中", i)
